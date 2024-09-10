4b8adc37427b8d12ab95a290d374a4d8fe9b3a88
ray <audi3m@naver.com>
2024-09-10 22:31:57 +0900

    Initial Commit

---
+++ CarHistoryApp/CarHistoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarHistoryAppApp.swift
+//  CarHistoryApp
+//
+//  Created by J Oh on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CarHistoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
