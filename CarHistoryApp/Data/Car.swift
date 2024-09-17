fec9b125a2564510d81e9afeadca28ce873d8511
ray <audi3m@naver.com>
2024-09-17 23:11:48 +0900

    데이터 모델

---
+++ CarHistoryApp/Data/Car.swift
@@ -0,0 +1,56 @@
+//
+//  Car.swift
+//  CarHistoryApp
+//
+//  Created by J Oh on 9/17/24.
+//
+
+import Foundation
+import RealmSwift
+import CoreLocation
+
+final class CarHistory: Object, ObjectKeyIdentifiable {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var date = Date()
+    @Persisted var historyType = HistoryType.refuel
+    @Persisted var content = ""
+    @Persisted var mileage = 0.0
+    @Persisted var latitude = 0.0
+    @Persisted var longitude = 0.0
+    @Persisted var cost = 0.0
+    @Persisted var price = 0.0
+    
+    @Persisted(originProperty: "history") var group: LinkingObjects<Car>
+    
+    
+    var toCLLocation: CLLocation {
+        return CLLocation(latitude: latitude, longitude: longitude)
+    }
+}
+
+final class Car: Object, ObjectKeyIdentifiable {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var manufacturer = ""
+    @Persisted var name = ""
+    @Persisted var plateNumber = ""
+    @Persisted var fuelType = FuelType.gasoline
+    @Persisted var color = "" // 나중에
+    
+    @Persisted var historyList = RealmSwift.List<CarHistory>()
+    
+    
+}
+
+enum HistoryType: String, PersistableEnum, CaseIterable {
+    case refuel = "Refuel"
+    case maintenance = "Maint."
+    case carWash = "Car Wash"
+    case etc = "etc"
+}
+
+enum FuelType: String, PersistableEnum, CaseIterable {
+    case gasoline = "Gasoline"
+    case diesel = "Diesel"
+    case electric = "Electric"
+}
+ 
---
+++ CarHistoryApp/Data/DummyData.swift
@@ -0,0 +1,12 @@
+//
+//  DummyData.swift
+//  CarHistoryApp
+//
+//  Created by J Oh on 9/17/24.
+//
+
+import Foundation
+
+enum DummyData {
+    static let recent = [
+}
