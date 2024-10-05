7e1e1bab26524d7a18f49340bdf95c7bd537a651
ray <audi3m@naver.com>
2024-10-05 21:19:38 +0900

    월별 섹션 나누기

--- CarHistoryApp/Summary/MaintanenceView.swift
+++ CarHistoryApp/Summary/MaintanenceView.swift
@@ -7,23 +7,6 @@
 
 import SwiftUI
 
-/*
- ------------------------
- |  ------------------- |
- |  |                 | |
- |  |     < 달력 >     | |
- |  |                 | |
- |  ------------------  |
- |                      |
- |                      |
- | <        9월       >  |
- | - 정비 1              |
- | - 정비 2              |
- | - 세차 3              |
- |                      |
- ------------------------
- */
-
 struct MaintanenceView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
