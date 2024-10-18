71d5e7e0053b619dc5f19c59a185925deb9c9803
ray <audi3m@naver.com>
2024-10-18 21:25:49 +0900

    Chore: Maps 목적지 이름 적용

--- CarHistoryApp/Managers/LinkToAppManager.swift
+++ CarHistoryApp/Managers/LinkToAppManager.swift
@@ -10,24 +10,9 @@ import MapKit
 
 enum LinkToAppManager {
     
-//    static func openLocationInGoogleMaps(place: Place) {
-//        let urlString = "comgooglemaps://?q=\(place.latitude),\(place.longitude)"
-//        if let url = URL(string: urlString), UIApplication.shared.canOpenURL(url) {
-//            UIApplication.shared.open(url, options: [:], completionHandler: nil)
-//        } else {
-//            openLocationInBrowser(place: place)
-//        }
-//    }
-//    
-//    static func openLocationInBrowser(place: Place) {
-//        let urlString = "https://www.google.com/maps/search/?api=1&query=\(place.latitude),\(place.longitude)"
-//        if let url = URL(string: urlString) {
-//            UIApplication.shared.open(url, options: [:], completionHandler: nil)
-//        }
-//    }
-    
     static func openInAppleMaps(place: Place) {
         let mapItem = MKMapItem(placemark: place.placemark)
+        mapItem.name = place.name
         mapItem.openInMaps(launchOptions: [
             MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving
         ])
