addc9dcf3b8a577ed3aa794537716865dcc7dbcc
ray <audi3m@naver.com>
2024-10-25 21:20:32 +0900

    Refactor: 이미지 저장 클래스 enum으로 변경

--- CarHistoryApp/Managers/CarImageManager.swift
+++ CarHistoryApp/Managers/CarImageManager.swift
@@ -5,4 +5,60 @@
 //  Created by J Oh on 10/22/24.
 //
 
-import Foundation
+import SwiftUI
+
+// 자동차 이미지 저장
+enum CarImageManager {
+    
+    static func saveImageToDocument(image: UIImage, filename: String) {
+        
+        guard let documentDirectory = FileManager.default.urls(
+            for: .documentDirectory,
+            in: .userDomainMask).first else { return }
+        
+        let fileURL = documentDirectory.appendingPathComponent("\(filename).png")
+        
+        guard let data = image.pngData() else { return }
+        
+        do {
+            try data.write(to: fileURL)
+        } catch {
+            print("File save error", error)
+        }
+    }
+    
+    static func loadImageToDocument(filename: String) -> UIImage? {
+        
+        guard let documentDirectory = FileManager.default.urls(
+            for: .documentDirectory,
+            in: .userDomainMask).first else { return nil }
+        
+        let fileURL = documentDirectory.appendingPathComponent("\(filename).png")
+        
+        if FileManager.default.fileExists(atPath: fileURL.path()) {
+            return UIImage(contentsOfFile: fileURL.path())
+        } else {
+            return nil
+        }
+    }
+    
+    static func removeImageFromDocument(filename: String) {
+        guard let documentDirectory = FileManager.default.urls(
+            for: .documentDirectory,
+            in: .userDomainMask).first else { return }
+        
+        let fileURL = documentDirectory.appendingPathComponent("\(filename).png")
+        
+        if FileManager.default.fileExists(atPath: fileURL.path()) {
+            
+            do {
+                try FileManager.default.removeItem(atPath: fileURL.path())
+            } catch {
+                print("File remove error", error)
+            }
+            
+        } else {
+            print("File does not exist")
+        }
+    }
+}
