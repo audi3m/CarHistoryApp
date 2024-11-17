dca2550837a47a07e2c8c7610afa78555ae8bf86
ray <audi3m@naver.com>
2024-11-17 18:11:22 +0900

    Chore: 리스트 테스트뷰 구현

--- CarHistoryApp/Model/RepositoryTestView.swift
+++ CarHistoryApp/Model/RepositoryTestView.swift
@@ -8,11 +8,85 @@
 import SwiftUI
 
 struct RepositoryTestView: View {
+    @EnvironmentObject var dataManager: AllDataManager
+    
+    @State private var showAddNewCarSheet = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                ForEach(dataManager.logs) { log in
+                    Text("\(Int.random(in: 100...999))")
+                    
+//                    VStack {
+//                        Text("\(log.totalCost)")
+//                        Text(log.companyName)
+//                        Text(log.date.description)
+//                        Text(log.logType.rawValue)
+//                    }
+                }
+            }
+            .navigationTitle("Test")
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    carSelector()
+                }
+            }
+        }
+        .onAppear {
+            dataManager.fetchCars()
+            if let car = dataManager.fetchSelectedCar() {
+                dataManager.fetchLogs(carID: car.id)
+            }
+        }
     }
 }
 
-#Preview {
-    RepositoryTestView()
+extension RepositoryTestView {
+    @ViewBuilder
+    private func carSelector() -> some View {
+        if dataManager.cars.isEmpty {
+            Button {
+                showAddNewCarSheet = true
+            } label: {
+                HStack {
+                    Text("차량 등록")
+                    Image(systemName: "plus.circle")
+                }
+            }
+        } else {
+            Menu {
+                Section {
+                    ForEach(dataManager.cars) { car in
+                        Button {
+                            dataManager.selectCar(car: car)
+                        } label: {
+                            Text(car.plateNumber)
+                            if let selectedCar = dataManager.fetchSelectedCar(), selectedCar == car {
+                                Image(systemName: "checkmark")
+                            }
+                        }
+                    }
+                }
+                
+                Section{
+                    Button {
+                        showAddNewCarSheet = true
+                    } label: {
+                        Label("추가하기", systemImage: "plus.circle")
+                    }
+                }
+                
+            } label: {
+                HStack {
+                    Text(dataManager.selectedCar?.plateNumber ?? "None")
+                        .font(.title2)
+                        .bold()
+                    Image(systemName: "chevron.down")
+                        .font(.caption)
+                }
+                .foregroundStyle(.blackWhite)
+            }
+        }
+    }
 }
