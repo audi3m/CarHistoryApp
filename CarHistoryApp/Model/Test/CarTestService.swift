c90759a084138d84c290d08e3e3993de3662320d
ray <audi3m@naver.com>
2024-11-17 18:26:25 +0900

    Feat: 테스트를 위한 객체 구현

--- CarHistoryApp/Model/Test/CarTestService.swift
+++ CarHistoryApp/Model/Test/CarTestService.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+final class CarTestService: CarRepository {
+    // Test
+    
+}
+
+extension CarTestService {
+    
+    func fetchCars() -> [CarDomain] {
+        return []
+    }
+    
+    func fetchCarOfInterest(carID: String) -> CarDomain? {
+        return nil
+    }
+    
+    func createCar(car: CarDomain) {
+        
+    }
+    
+    func deleteCar(car: CarDomain) {
+        
+    }
+    
+    func updateCar(car: CarDomain) {
+        
+    }
+    
+}
+
--- CarHistoryApp/Model/Test/LogTestService.swift
+++ CarHistoryApp/Model/Test/LogTestService.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+final class LogTestService: LogRepository {
+    // Test
+    
+}
+
+extension LogTestService {
+     
+    func fetchLogs(carID: String) -> [LogDomain] {
+        return []
+    }
+    
+    func createLog(log: LogDomain) {
+        
+    }
+    
+    func deleteLog(logID: String) {
+        
+    }
+    
+    func updateLog(log: LogDomain) {
+        
+    }
+    
+}
