9a570b832ac1b0a9963c77ee2327047925c5bf11
ray <audi3m@naver.com>
2024-12-24 22:36:19 +0900

    Add: 간략 설명 추가, 연료 가격 계산 추가

---
+++ CarHistoryApp/etc/LogCell.swift
@@ -0,0 +1,18 @@
+//
+//  LogCell.swift
+//  CarHistoryApp
+//
+//  Created by J Oh on 12/24/24.
+//
+
+import SwiftUI
+
+struct LogCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogCell()
+}
---
+++ CarHistoryApp/etc/PriceCell.swift
@@ -0,0 +1,18 @@
+//
+//  PriceCell.swift
+//  CarHistoryApp
+//
+//  Created by J Oh on 12/24/24.
+//
+
+import SwiftUI
+
+struct PriceCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PriceCell()
+}
