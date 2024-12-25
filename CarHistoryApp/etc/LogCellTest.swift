39cd8e457be4e740e51b645fceacd6bde9375f3b
ray <audi3m@naver.com>
2024-12-25 21:12:11 +0900

    Design: 셀 디자인 변경

--- CarHistoryApp/etc/LogCellTest.swift
+++ CarHistoryApp/etc/LogCellTest.swift
@@ -8,11 +8,49 @@
 import SwiftUI
 
 struct LogCellTest: View {
+    
+    let log: LogDomain
+    
+    @State private var expanded = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            // 앞 표시
+            RoundedRectangle(cornerRadius: 8)
+                .frame(width: 2.5)
+                .foregroundStyle(log.typeColor)
+                .padding(.vertical, 10)
+            
+            Image(systemName: log.logType.image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 20, height: 20)
+                .padding(5)
+            
+            // 상호+가격
+            VStack(alignment: .leading) {
+                Text(log.companyName)
+                    .font(.footnote)
+                
+                Text(log.subDescription)
+                    .font(.caption)
+                    .foregroundStyle(.secondary)
+            }
+            
+            Spacer()
+            Text(DateHelper.shortFormat(date: log.date))
+                .font(.caption)
+            
+        }
+        .frame(height: 60)
+        .padding(.horizontal, 10)
+        .background(.gray.opacity(0.1))
+        .clipShape(RoundedRectangle(cornerRadius: 8))
+        .listRowBackground(Color.clear)
     }
 }
 
+
 #Preview {
-    LogCellTest()
+    LogCellTest(log: .randomLog())
 }
--- CarHistoryApp/etc/PriceCell.swift
+++ CarHistoryApp/etc/PriceCell.swift
@@ -8,11 +8,26 @@
 import SwiftUI
 
 struct PriceCell: View {
+    
+    var price: Double
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment: .top, spacing: 16) {
+            Image(systemName: "wonsign.circle")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.blackWhite.opacity(0.7))
+                .frame(width: 23, height: 23)
+            
+            Spacer()
+            
+            HStack {
+                Text("리터당")
+                Text("\(Int(price))")
+                Text("원")
+            }
+            .frame(minHeight: 23)
+        }
+        .foregroundStyle(.secondary)
     }
-}
-
-#Preview {
-    PriceCell()
 }
