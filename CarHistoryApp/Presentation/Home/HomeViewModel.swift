52c7cecb0a1996afa18c73b3670aaded0d5b2d6a
ray <audi3m@naver.com>
2024-12-26 23:00:22 +0900

    Refactor: 홈 화면에서 보여줄 데이터 계산속성으로 변경

--- CarHistoryApp/Presentation/Home/HomeViewModel.swift
+++ CarHistoryApp/Presentation/Home/HomeViewModel.swift
@@ -11,21 +11,46 @@ import Combine
 final class HomeViewModel: ObservableObject {
     
     @Published var cars: [CarDomain] = []
-    @Published var recentFive: [LogDomain] = []
-    @Published var fuelCost: String = ""
-    @Published var lastWash: String = ""
-    
-    private var cancellables: Set<AnyCancellable> = []
+    @Published var selectedCar: CarDomain? 
     
     private let dataManager: LocalDataManager
+    private var cancellables = Set<AnyCancellable>()
+    
+    var recentFiveLogs: [LogDomain] {
+        dataManager.logs.suffix(5).reversed()
+    }
+    
+    var lastWash: String {
+        if let wash = dataManager.logs.last(where: { $0.logType == .carWash }) {
+            return wash.date.toSep30()
+        } else {
+            return "기록 없음"
+        }
+    }
+    
+    var fuelExpense: String {
+        let calendar = Calendar.current
+        let currentYear = calendar.component(.year, from: Date())
+        let currentMonth = calendar.component(.month, from: Date())
+        
+        let refuelList = dataManager.logs
+            .filter { $0.logType == .refuel }
+            .filter {
+                let logYear = calendar.component(.year, from: $0.date)
+                let logMonth = calendar.component(.month, from: $0.date)
+                return logYear == currentYear && logMonth == currentMonth
+            }
+        
+        let totalCost = refuelList.reduce(0) { $0 + $1.totalCost }
+        
+        return "₩\(Int(totalCost).formatted())"
+    }
+    
     
     init(dataManager: LocalDataManager) {
         self.dataManager = dataManager
-        
         assignCars()
-        assignRecentFive()
-        assignLastWash()
-        
+        selectedCar = dataManager.selectedCar
         print("init HomeViewModel")
     }
     
@@ -42,40 +67,16 @@ extension HomeViewModel {
             .store(in: &cancellables)
     }
     
-    private func assignRecentFive() {
-        dataManager.$logs
-            .removeDuplicates()
-            .map { logs in
-                logs.suffix(5).reversed()
-            }
-            .assign(to: \.recentFive, on: self)
-            .store(in: &cancellables)
-    }
-    
-    private func getFuelCost() {
-        
-    }
-    
-    private func assignLastWash() {
-        dataManager.$logs
-            .removeDuplicates()
-            .map { [weak self] logs -> String in
-                guard let self else { return "" }
-                return self.getLastWash(logs: logs)
-            }
-            .assign(to: \.lastWash, on: self)
-            .store(in: &cancellables)
+    private func cancelCombine() {
+        cancellables.removeAll()
     }
     
 }
 
 extension HomeViewModel {
-    
-    private func getLastWash(logs: [LogDomain]) -> String {
-        if let wash = logs.last(where: { $0.logType == .carWash }) {
-            return wash.date.toSep30()
-        } else {
-            return "기록 없음"
-        }
+    func selectCar(car: CarDomain) {
+        BasicSettingsHelper.selectedCarNumber = car.plateNumber
+        selectedCar = car
+        dataManager.selectCar(car: car)
     }
 }
